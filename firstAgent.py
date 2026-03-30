b665e991ed4d02229b9ad953afb33878011d23cd
Vamsi <mvamsikrishna982@gmail.com>
2026-03-31 00:01:59 +0530

    created first aget with locla tool

---
+++ firstAgent.py
@@ -0,0 +1,34 @@
+from dotenv import load_dotenv
+import os
+from langchain_groq import ChatGroq
+from langchain.agents import create_agent
+from langchain_core.messages import HumanMessage
+from langchain_core.tools import tool
+load_dotenv()
+
+@tool
+def search(query:str)->str:
+    """
+    Returns the current weather for a chicago city.
+    Use this tool when the user asks about weather.
+    Agrs:
+        query: The query to search for.
+    Returns:
+        The search result
+    """
+    print("This is the input query:",query)
+    return "chicago is very sunny today"
+
+
+
+llm = ChatGroq(   
+    model="llama-3.3-70b-versatile",
+    api_key = os.getenv('GROQ_API_KEY')
+    )
+
+tools = [search]
+agent = create_agent(model=llm, tools=tools)
+
+result= agent.invoke({'messages':[HumanMessage(content="How is the weather in chicago today")]})
+print(result)
+print(type(result))
